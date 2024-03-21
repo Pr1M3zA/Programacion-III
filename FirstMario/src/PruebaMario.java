2461f5376ece3444d42986d4766dfe857e318572
Pr1M3zA <priscilav.mezal@gmail.com>
2024-03-21 16:35:46 -0600

    Java Graph2D Prueba

---
+++ FirstMario/src/PruebaMario.java
@@ -0,0 +1,15 @@
+
+import javax.swing.JFrame;
+
+public class PruebaMario {
+
+	public static void main(String[] args) {
+        JFrame frame = new JFrame("Java 2D Graphics");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.setLayout(null);
+        //frame.setBounds(0, 0, 974, 683);
+        frame.setSize(986, 690);
+        frame.add(new FirstWorld());
+        frame.setVisible(true);
+	}
+}
